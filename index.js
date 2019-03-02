b0329ef4b51322c5b086fe0f6b5571de0050003b
Vlado <selvo.selvo@gmail.com>
2019-03-02 21:23:49 +0100

    post, put, nije ok

--- index.js
+++ index.js
@@ -13,4 +13,24 @@ app.get('/api/player/Matko', (req, res) => res.send([{ime:"Matko", brojIgara:'34
 
 app.get('/api/place', (req, res) => res.send({ ime:'webgame', adresa:'Svetice', radnoVrijeme: '8-16'}));
 
+//POST /api
+
+
+
+app.post('/api/game', (req, res) => res.send([{imeIgraca:"Pero"}, {imeIgraca:"Ivo"},{imeIgraca:"Kruno"}], { lokacija:'Dugave'}, {id:'125'}));
+app.post('/api/user', (req, res) => res.send([{ime:"Toni", id:'65'}, {ime:"Dino", id:'17'},{ime:"Ronald", id:'90'}]));
+app.post('/api/player/Ray', (req, res) => res.send([{ime:"Ray", brojIgara:'560', avgBodova:'17'}]));
+app.post('/api/player/Charles', (req, res) => res.send([{ime:"Charles", brojIgara:'415', avgBodova:'21'}]));
+app.post('/api/player/Louis', (req, res) => res.send([{ime:"Louis", brojIgara:'3467', avgBodova:'33'}]));
+app.post('/api/place', (req, res) => res.send({ ime:'drugaigra', adresa:'Trnsko', radnoVrijeme: '8-16'}));
+
+app.put('/api/game', (req, res) => res.send([{imeIgraca:"Pero"}, {imeIgraca:"Ivo"},{imeIgraca:"Kruno"}], { lokacija:'Dugave'}, {id:'125'}));
+app.put('/api/user', (req, res) => res.send([{ime:"Toni", id:'65'}, {ime:"Dino", id:'17'},{ime:"Ronald", id:'90'}]));
+app.put('/api/player/Ray', (req, res) => res.send([{ime:"Ray", brojIgara:'560', avgBodova:'17'}]));
+app.put('/api/player/Charles', (req, res) => res.send([{ime:"Charles", brojIgara:'415', avgBodova:'21'}]));
+app.put('/api/player/Louis', (req, res) => res.send([{ime:"Louis", brojIgara:'3467', avgBodova:'33'}]));
+app.put('/api/place', (req, res) => res.send({ ime:'drugaigra', adresa:'Trnsko', radnoVrijeme: '8-16'}));
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
